fc55f75c12f028edc6d1ff5b4aedfe2ae847ca62
Rohith <rohithgupta@gmail.com>
2019-03-10 13:30:38 -0400

    Adding uploader

---
+++ PlayAround/complete/complete/server/src/main/java/filestorageserver/FileUploadController.java
@@ -0,0 +1,36 @@
+package filestorageserver;
+
+import filestorageserver.storage.StorageFileNotFoundException;
+import filestorageserver.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        return "You successfully uploaded " + file.getOriginalFilename() + "!";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+}
